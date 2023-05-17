72733fb910427743277e6645035910005e0ec6b8
malbaladejo <malbaladejo@free.fr>
2023-05-17 17:56:11 +0200

    V3: jeune genevois, background bicolor

--- src/app/services/specialdays.service.ts
+++ src/app/services/specialdays.service.ts
@@ -11,6 +11,7 @@ export class SpecialDaysService {
   private ascensionDates = new Map<number, Date>();
   private ascensionFridayDates = new Map<number, Date>();
   private pentecoteDates = new Map<number, Date>();
+  private jeuneGenevoisDates = new Map<number, Date>();
 
   public getLabel(date: Date): SpecialDay | null {
     const day = date.getDate();
@@ -82,6 +83,11 @@ export class SpecialDaysService {
       return new SpecialDay("PENTECOTE", true, true);
     }
 
+    this.buildJeuneGenevois(year);
+    if (this.dateEquals(date, this.jeuneGenevoisDates.get(year))) {
+      return new SpecialDay("JEUNE GENEVOIS", false, true);
+    }
+
     return null;
   }
 
@@ -103,15 +109,27 @@ export class SpecialDaysService {
     const a = year % 19
     const century = Math.floor(year / 100)
     const yearsAfterCentury = year % 100
-    const d =
-      (19 * a + century - Math.floor(century / 4) - Math.floor((Math.floor(century - (century + 8) / 25) + 1) / 3) + 15) %
-      30
+    const d = (19 * a + century - Math.floor(century / 4) - Math.floor((Math.floor(century - (century + 8) / 25) + 1) / 3) + 15) % 30
     const e = (32 + 2 * (century % 4) + 2 * Math.floor(yearsAfterCentury / 4) - d - (yearsAfterCentury % 4)) % 7
     const f = d + e - 7 * Math.floor((a + 11 * d + 22 * e) / 451) + 114
     const month = Math.floor(f / 31)
     const day = (f % 31) + 1
 
     return new Date(year, month - 1, day)
+  }
+
+  private buildJeuneGenevois(year: number) {
+    let date = this.jeuneGenevoisDates.get(year);
+    if (date) {
+      return;
+    }
+
+    const firstSeptember = new Date(year, 8, 1);
+    const dayOfFirstSeptember = firstSeptember.getDay();
+    const firstSundayInNDays = (7 - dayOfFirstSeptember) % 7;
+    const jeuneGenevoisInNDays = firstSundayInNDays + 4;
+    const jeuneGenevois = new Date(year, 8, 1 + jeuneGenevoisInNDays);
+    this.jeuneGenevoisDates.set(year, jeuneGenevois);
   }
 
   private addDays(date: Date | undefined, days: number): Date {
